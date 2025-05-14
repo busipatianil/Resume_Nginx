9de6df3949b853918650eb5021456a8ce0e4e880
busipatianil <155179624+busipatianil@users.noreply.github.com>
2025-05-14 15:54:35 +0530

    pythonscripts.py

---
+++ pythonscripts.py
@@ -0,0 +1,13 @@
+#########
+#Print 5 series numbers
+for i in range(5):
+    print ("Number",i)
+########################
+
+#if else loop
+x=10
+x > 20
+  print("x greaterthan 20")
+else:
+   print("x lessthan 20")
+
